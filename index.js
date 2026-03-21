00735c93b358c0e53583764ebea9c146d90d0b95
Вардан <vardan@MacBook-Air-Vardan.local>
2026-03-21 11:32:56 +0300

    $

--- index.js
+++ index.js
@@ -1311,7 +1311,7 @@ async function sendTheophanMessage() {
 }
 
 // Каждый день в 11:00 по серверному времени
-schedule.scheduleJob({ tz: 'Europe/Moscow', hour: 13, minute: 1 }, sendTheophanMessage);
+schedule.scheduleJob({ tz: 'Europe/Moscow', hour: 11, minute: 36 }, sendTheophanMessage);
 console.log('✅ Расписание мысли дня установлено на 12:48 MSK');
 
 
